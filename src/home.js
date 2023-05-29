7217a6a4700a5572e102dbd0a395f1f334af10ae
alihanayaz <alihanayaz0@gmail.com>
2023-05-29 22:56:53 +0300

    fix typo

--- src/home.js
+++ src/home.js
@@ -2,7 +2,6 @@ const createHome = () => {
   const main = document.querySelector('.main');
   const home = document.createElement('div');
   home.classList.add('home');
-  home.textContent = 'Home';
   main.appendChild(home);
 };
 
